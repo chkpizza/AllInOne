d1d577aa913bdaf2bbd7287def8f4f23b073229a
chkpizza <48915641+chkpizza@users.noreply.github.com>
2023-09-04 22:09:41 +0900

    BottomNavigationView, NavigationBar 색상 변경

--- base/src/main/java/com/wantique/base/ui/BaseActivity.kt
+++ base/src/main/java/com/wantique/base/ui/BaseActivity.kt
@@ -1,9 +1,17 @@
 package com.wantique.base.ui
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.updateLayoutParams
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
@@ -26,6 +34,12 @@ open class BaseActivity<T: ViewDataBinding>(
 
     private fun setUpEdgeToEdge() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = true
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
+            WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightNavigationBars = true
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR or  View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
     }
 }
