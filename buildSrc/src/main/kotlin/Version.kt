7085e0ad938195b7802da3da56a67cda43a94a9a
chkpizza <48915641+chkpizza@users.noreply.github.com>
2023-09-11 20:57:16 +0900

    gradle plugin, kotlin 버전 업그레이드

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -2,5 +2,5 @@ object Version {
     const val compileSdk = 33
     const val targetSdk = 33
     const val minSdk = 26
-    const val buildToolsVersion = "30.0.3"
+    const val buildToolsVersion = "33.0.1"
 }
