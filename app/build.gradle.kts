b65b3af338f5b38665d493f02d547d5ea99ded15
chkpizza <48915641+chkpizza@users.noreply.github.com>
2023-09-11 17:04:04 +0900

    광고 ID 권한 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.wantique.allinone"
         minSdk = Version.minSdk
         targetSdk = Version.targetSdk
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
