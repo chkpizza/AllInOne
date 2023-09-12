aba984d27274e81eb675328818d7e2b4a09c40ef
chkpizza <48915641+chkpizza@users.noreply.github.com>
2023-09-12 13:03:18 +0900

    버전 코드 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.wantique.allinone"
         minSdk = Version.minSdk
         targetSdk = Version.targetSdk
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
